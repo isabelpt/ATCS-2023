4c6c7e562cbb97786c8ccca2b50449e44814ed69
Isabel Prado-Tucker <56853689+isabelpt@users.noreply.github.com>
2023-12-09 14:43:33 -0800

    Add files via upload

--- Sem1FinalProject/food.py
+++ Sem1FinalProject/food.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from fsm import FSM
 
 class Food:
     def __init__(self, screen_width, screen_height, num_food):
@@ -9,11 +10,20 @@ class Food:
         self.reveal_dist = 70
         self.generate_food(screen_width, screen_height, num_food)
 
+        self.fsm = FSM(0)
+        self.init_fsm()
+
+        self.states = {0, 1}
+
     def generate_food(self, screen_width, screen_height, num_food):
         for _ in range(num_food):
             food_pos = (random.randint(-screen_width, 2 * screen_width),
                         random.randint(-screen_height, 2 * screen_height))
             self.food_list.append(food_pos)
+    
+    def init_fsm(self):
+        # self.fsm.add_transition(" ", 0, self., "S")
+        pass
     
     def update_reveal_dist(self, change):
         self.reveal_dist = max(10, self.reveal_dist - change)
